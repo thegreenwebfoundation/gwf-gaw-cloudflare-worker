d95a6eec7dcf1bcfb860f6d9d25fa500f09eaa41
fershad <27988517+fershad@users.noreply.github.com>
2025-02-02 16:11:40 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,79 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { gridAwarePower } from '@greenweb/grid-aware-websites';
+import { getLocation } from '@greenweb/gaw-plugin-cloudflare-workers';
 
 export default {
+	// First fetch the request
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const response = await fetch(request.url);
+		// Then check if the request content type is HTML.
+		const contentType = response.headers.get('content-type');
+
+		// If the content is not HTML, then return the response without any changes.
+		if (!contentType || !contentType.includes('text/html')) {
+			return new Response(response.body, {
+				...response,
+			});
+		}
+
+		try {
+			// If the content type is HTML, we get the country the request came from
+			const location = await getLocation(request);
+			const { country } = location;
+
+			// If the country data does not exist, then return the response without any changes.
+			if (!country) {
+				return new Response(response.body, {
+					...response,
+				});
+			}
+
+			const gridData = await gridAwarePower(country, env.EMAPS_API_KEY, {
+				mode: 'low-carbon'
+			});
+
+			// If there's an error getting data, return the web page without any modifications
+			if (gridData.status === 'error') {
+				return new Response(response.body, {
+					...response,
+					headers: {
+						...response.headers,
+					},
+				});
+			}
+
+			// If the grid aware flag is triggered (gridAware === true), then we'll return a modified HTML page to the user.
+			if (gridData.gridAware) {
+				const modifyHTML = new HTMLRewriter().on('iframe', {
+					element(element) {
+						element.remove();
+					},
+				});
+
+				// Transform the response using the HTMLRewriter API, and set appropriate headers.
+				let modifiedResponse = new Response(modifyHTML.transform(response).body, {
+					...response,
+					headers: {
+						...response.headers,
+						'Content-Type': 'text/html;charset=UTF-8',
+					},
+				});
+
+				return modifiedResponse;
+			}
+
+			return new Response(response.body, {
+				...response,
+				headers: {
+					...response.headers,
+				},
+			});
+		} catch (e) {
+			return new Response(response.body, {
+				...response,
+				headers: {
+					...response.headers,
+				},
+			});
+		}
 	},
 };
